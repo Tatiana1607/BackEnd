541300b89b46426481aaa9c458e37a26bc54b796
alumnos nunoa <alumnosnunoa@inacap.cl>
2025-09-01 19:56:53 -0400

    Mejoras en modelos

--- miapp/admin.py
+++ miapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Cliente, Empleado, OrdenCompra, EstadoCompra
 
 # Register your models here.
+admin.site.register(Cliente)
+admin.site.register(Empleado)
+admin.site.register(OrdenCompra)
+admin.site.register(EstadoCompra)
--- miapp/models.py
+++ miapp/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class EstadoCompra(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
+class Cliente(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    correo = models.EmailField(unique=True)
+    telefono = models.CharField(max_length=20, blank=True)
+    direccion = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.nombre
+
+class Empleado(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    correo = models.EmailField(unique=True)
+    cargo = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.nombre} - {self.cargo}"
+
+class OrdenCompra(models.Model):
+    id = models.AutoField(primary_key=True)
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    empleado = models.ForeignKey(Empleado, on_delete=models.SET_NULL, null=True)
+    fecha = models.DateTimeField(auto_now_add=True)
+    estado = models.ForeignKey(EstadoCompra, on_delete=models.SET_NULL, null=True)
+    total = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"Orden #{self.id} - {self.cliente.nombre}"
